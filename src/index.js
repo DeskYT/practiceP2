4ab3ce9f81b93d99c33962026c82a70005cb3651
DeskYT <deskytube@gmail.com>
2020-10-19 17:45:39 +0300

    students axios released

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import Vue from 'vue'
 import axios from 'axios'
-import VueAxios from 'vue-axios'
-//Vue.use(VueAxios, axios)
-
 
 new Vue({
     el: '#app',
     data: {
         students: [],
-        search: ''
+        coincidence: ""
     },
     mounted: function (){
         axios.get("http://46.101.212.195:3000/students").then(response => {
            console.log(response.data)
            this.students = response.data
-            console.log(this.students)
         });
+    },
+    updated() {
+        console.log(this.coincidence)
     }
 })
